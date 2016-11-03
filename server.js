e586c676d352e461105aedfc3052ddfb50b09600
Mark Ferris <littleferris@gmail.com>
2016-11-02 21:25:25 -0600

    added some details

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//---------Req---------
 var express = require('express');
 var bodyParser = require('body-parser');
 var massive = require('massive');
@@ -15,8 +16,9 @@ var massiveinstance = massive.connectSync({connectionString : connectionString})
 app.set('db', massiveinstance);
 var db = app.get('db');
 
-var servCtrl = require('./servCtrl');
 
+var servCtrl = require('./servCtrl');
+//---------- EndPoints -----------
 app.post('/products', servCtrl.create);
 app.get('/products', servCtrl.getAll);
 app.get('/products/:id', servCtrl.getOne);
@@ -24,6 +26,7 @@ app.put('/products/update/description/:id', servCtrl.update_description);
 app.put('/products/update/price/:id', servCtrl.update_price);
 app.delete('/products/delete/:id', servCtrl.destroy);
 
+//----------Server------------
 app.listen('3232', function(){
   console.log("Successfully listening on port 3232")
 });
